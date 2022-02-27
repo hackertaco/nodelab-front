5e2d316809b76adede85d526a9c29618e3854450
hackertaco <imsa1224@korea.ac.kr>
2022-02-27 23:58:06 +0900

    Style: Add Main Image

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <Tutorial />
+  <index></index>
 </template>
 
 <script>
+import Index from '~/components/main'
+
 export default {
   name: 'IndexPage',
+  components: { Index }
 }
 </script>
