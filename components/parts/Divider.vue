2d42226d4132d1c066a206f3a6e426a443330212
hackertaco <imsa1224@korea.ac.kr>
2022-03-08 22:12:33 +0900

    style: Dynamic Divider

--- components/parts/Divider.vue
+++ components/parts/Divider.vue
@@ -1,14 +1,21 @@
 <template>
   <div class='divider-container'>
-    <div class='main-line'></div>
-    <div class='right-rect'></div>
+    <div :class='`${color}-line`'></div>
+    <div :class='`${color}-rect`'></div>
     <div class='main-rect'></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DividerLine'
+  name: 'DividerLine',
+  props: {
+    color: {
+      type: String,
+      default: 'main'
+    }
+  }
+
 }
 </script>
 
@@ -23,16 +30,30 @@ export default {
   border: 2px solid #191919;
 }
 
+.grey-line {
+  width: 100%;
+  border: 2px solid $grey-3;
+}
+
 .main-rect {
   width: 100%;
   background-color: #F7f7f7;
-  height: 30px;
+  height: 2vh;
+}
+
+.grey-rect {
+  background: $grey-3;
+  width: 8%;
+  height: 1vh;
+  position: absolute;
+  right: 0;
+  z-index: 10;
 }
 
 .right-rect {
   background: #191919;
   width: 8%;
-  height: 12px;
+  height: 1vh;
   position: absolute;
   right: 0;
   z-index: 10;
