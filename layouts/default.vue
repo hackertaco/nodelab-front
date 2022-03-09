98d221a32a8c752c312b0b601c9bf72cd5429833
hackertaco <imsa1224@korea.ac.kr>
2022-03-09 16:26:17 +0900

    refactor: Divide Search Modal from Default Header Layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,34 +17,7 @@
       </div>
     </div>
     <div v-if='isSearching' class='recommend-container'>
-      <div class='recommend-contents'>
-        <div class='recommend-title'>
-          <icon-star></icon-star>
-          <span>추천 카테고리</span>
-          <div class='rectangular'></div>
-        </div>
-        <divider-line class='divider' color='grey'></divider-line>
-        <div class='recommend-category'>
-          <div class='item'>
-            <div class='btn'>카테고리</div>
-            <div class='line'>
-            </div>
-          </div>
-          <div class='item'>
-            <div class='btn'>카테고리</div>
-            <div class='line'></div>
-          </div>
-          <div class='item'>
-            <div class='btn'>카테고리</div>
-            <div class='line'></div>
-          </div>
-          <div class='item'>
-            <div class='btn'>카테고리</div>
-            <div class='line'></div>
-          </div>
-        </div>
-      </div>
-
+      <recommend-container></recommend-container>
     </div>
     <div v-if='isSearching' class='theOther-container' @click='isSearching=false'></div>
     <Nuxt />
@@ -56,18 +29,20 @@ import IconSearch from 'assets/svg/IconSearch'
 import IconMenu from 'assets/svg/IconMenu'
 import IconPrev from 'assets/svg/IconPrev'
 import { ref } from '@nuxtjs/composition-api'
-import IconStar from 'assets/svg/IconStar'
-import DividerLine from '~/components/parts/Divider'
+// import IconStar from 'assets/svg/IconStar'
+// import DividerLine from '~/components/parts/Divider'
+import RecommendContainer from '~/components/header/RecommendContainer'
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'default.vue',
-  components: { DividerLine, IconStar, IconPrev, IconMenu, IconSearch },
+  components: { RecommendContainer, IconPrev, IconMenu, IconSearch },
   setup() {
     const isSearching = ref(false)
     const toggleBtn = () => {
       isSearching.value = !isSearching.value
     }
+
 
     return {
       isSearching, toggleBtn
@@ -180,82 +155,82 @@ input[type='text'] {
   justify-content: center;
   top: 6vh;
 
-  .recommend-contents {
-    width: 62.5%;
-    display: flex;
-    flex-direction: column;
-
-    .divider {
-      margin-bottom: 6vh;
-    }
-
-    .recommend-category {
-      display: flex;
-
-      .item {
-        display: flex;
-        flex-direction: column;
-        margin-right: 1.25vw;
-        align-content: center;
-        cursor: pointer;
-
-        .btn {
-          background-color: $grey-1;
-          width: 6vw;
-          height: 4vh;
-          margin-bottom: 5px;
-          font-size: $kor-20;
-          color: $grey-4;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-
-        .line {
-          border-bottom: 1px solid #E5E5E5;
-        }
-      }
-
-      .item:hover {
-
-        .btn {
-          color: $pink;
-          font-weight: bold;
-          margin-bottom: 0;
-        }
-
-        .line {
-          border-bottom: 1px solid $pink;
-        }
-      }
-    }
-
-    .recommend-title {
-      display: flex;
-      align-items: center;
-      position: relative;
-      top: 4vh;
-      margin-bottom: 6vh;
-
-      span {
-        margin-left: 0.9vw;
-        font-size: $kor-36;
-        line-height: 49px;
-        font-weight: bold;
-      }
-
-      .rectangular {
-        width: 8px;
-        height: 8px;
-        background-color: $dark-191919;
-        margin-top: 7px;
-        margin-left: 0.2vw;
-        position: relative;
-        bottom: 25%;
-      }
-    }
-
-  }
+  //.recommend-contents {
+  //  width: 62.5%;
+  //  display: flex;
+  //  flex-direction: column;
+  //
+  //  .divider {
+  //    margin-bottom: 6vh;
+  //  }
+  //
+  //  .recommend-category {
+  //    display: flex;
+  //
+  //    .item {
+  //      display: flex;
+  //      flex-direction: column;
+  //      margin-right: 1.25vw;
+  //      align-content: center;
+  //      cursor: pointer;
+  //
+  //      .btn {
+  //        background-color: $grey-1;
+  //        width: 6vw;
+  //        height: 4vh;
+  //        margin-bottom: 5px;
+  //        font-size: $kor-20;
+  //        color: $grey-4;
+  //        display: flex;
+  //        align-items: center;
+  //        justify-content: center;
+  //      }
+  //
+  //      .line {
+  //        border-bottom: 1px solid #E5E5E5;
+  //      }
+  //    }
+  //
+  //    .item:hover {
+  //
+  //      .btn {
+  //        color: $pink;
+  //        font-weight: bold;
+  //        margin-bottom: 0;
+  //      }
+  //
+  //      .line {
+  //        border-bottom: 1px solid $pink;
+  //      }
+  //    }
+  //  }
+  //
+  //  .recommend-title {
+  //    display: flex;
+  //    align-items: center;
+  //    position: relative;
+  //    top: 4vh;
+  //    margin-bottom: 6vh;
+  //
+  //    span {
+  //      margin-left: 0.9vw;
+  //      font-size: $kor-36;
+  //      line-height: 49px;
+  //      font-weight: bold;
+  //    }
+  //
+  //    .rectangular {
+  //      width: 8px;
+  //      height: 8px;
+  //      background-color: $dark-191919;
+  //      margin-top: 7px;
+  //      margin-left: 0.2vw;
+  //      position: relative;
+  //      bottom: 25%;
+  //    }
+  //  }
+  //
+  //}
 }
 
 .theOther-container {
