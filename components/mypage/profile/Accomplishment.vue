277a465826055c8f8c192bb6a889fc7942c1c5b3
hackertaco <imsa1224@korea.ac.kr>
2022-03-12 23:35:35 +0900

    style: Add Study List

--- components/mypage/profile/Accomplishment.vue
+++ components/mypage/profile/Accomplishment.vue
@@ -4,24 +4,46 @@
       <profile-title>
         <template #title>평균 성취율</template>
       </profile-title>
-      <div class='study'>총 2개의 스터디</div>
+      <div v-if='!isStudyListOpened' class='study' @click='toggleStudyList'>총 2개의 스터디</div>
+      <div v-else class='study' @click='toggleStudyList'>목록 닫기</div>
     </div>
-    <div class='acc-info'>
+    <div v-if='!isStudyListOpened' class='acc-info'>
       <div class='acc-info-ratio-title'>성취율</div>
       <div class='acc-info-ratio'>80%</div>
     </div>
-    <div class='acc-ratio'>
+    <div v-if='!isStudyListOpened' class='acc-ratio'>
       <div class='acc-ratio-real'></div>
+    </div>
+    <div v-else class='acc-study-list'>
+      <div v-for='(list, index) in lists' :key='index' class='acc-study-list-container'>
+        <span class='list-title'>{{ list.name }}</span>
+        <span class='list-duration'>{{ list.duration }}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from '@nuxtjs/composition-api'
 import ProfileTitle from '~/components/mypage/profile/ProfileTitle'
 
 export default {
   name: 'AccomplishmentContainer',
-  components: { ProfileTitle }
+  components: { ProfileTitle },
+  setup() {
+    const isStudyListOpened = ref(false)
+    const toggleStudyList = () => {
+      isStudyListOpened.value = !isStudyListOpened.value
+    }
+    const lists = [
+      { name: '슈퍼하드 Processing Study', duration: '20. 10. 01 - 20. 11. 03' },
+      { name: '알고리즘 스터디', duration: '20. 11. 22 - 20. 12. 30' }
+    ]
+    return {
+      toggleStudyList,
+      isStudyListOpened, lists
+    }
+  }
 }
 </script>
 
@@ -78,5 +100,35 @@ export default {
 .study {
   color: $grey-4;
   font-size: $kor-20;
+  cursor: pointer;
+}
+
+.acc-study-list {
+  background: #FFFFFF;
+  border: 1px solid $grey-3;
+  width: 100%;
+  height: 22.9vh;
+  margin-top: 1.3vh;
+  font-family: "Chakra Petch", sans-serif;
+  display: flex;
+  flex-direction: column;
+  padding: 2vh 1vw 0 1vw;
+
+  &-container {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1.8vh;
+    align-items: center;
+  }
+
+}
+
+.list-title {
+  font-size: $kor-20;
+  font-weight: 500;
+}
+
+.list-duration {
+  font-size: $kor-p4;
 }
 </style>
