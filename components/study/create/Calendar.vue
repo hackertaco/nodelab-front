e7de40ac6dc7c44621bd70a3e23da0d35c2fca3e
hackertaco <imsa1224@korea.ac.kr>
2022-03-30 21:47:29 +0900

    Feat: Make Calendar

--- components/study/create/Calendar.vue
+++ components/study/create/Calendar.vue
@@ -1,31 +1,36 @@
 <template>
   <div class='calendar-main-container'>
     <div class='calendar-left'>
-      <div class='calendar-title'>
+      <div class='calendar-title' @click='goPrev'>
         <icon-prev class='icon-left' height='16' width='10'></icon-prev>
         {{ year }}.{{ month }}
       </div>
-      <div class='num'><span v-for='(d, i) in days' :key='i' :class='{ "red": d==="일"}' class='cal-item'>{{ d }}</span>
+      <div class='num'>
+
+        <span v-for='(d, i) in days' :key='i' :class='{ "red": d==="일"}' class='cal-item'>{{ d }}</span>
       </div>
       <div class='num'>
+        <span v-for='(b, idx) in blankLeft' :key='"blank"+idx'>{{ ' ' }}</span>
         <span
-          v-for='(l, i) in leftC' :key='i' :class='{"checked": isChecked(i, "left"), "between": isBetween(i, "left")}'
+          v-for='(l, i) in leftC' :key='i'
+          :class='{"checked": isChecked(year, Number(month), i), "between": isBetween(year, Number(month), i)}'
           class='cal-item'
-          @click='checkDay(i, "left")'>{{ l }}</span>
+          @click='checkDay(year, Number(month), i)'>{{ l }}</span>
       </div>
     </div>
     <div class='calendar-right'>
-      <div class='calendar-title right '>
-        {{ year }}.{{ nextMonth }}
-        <icon-next class='icon-right'></icon-next>
+      <div class='calendar-title right ' @click='goNext'>
+        {{ nextMonth === '01' ? year + 1 : year }}.{{ nextMonth }}
+        <icon-next class='icon-right '></icon-next>
       </div>
       <div class='num'><span v-for='(d, i) in days' :key='i' :class='{ "red": d==="일"}' class='cal-item'>{{ d }}</span>
       </div>
       <div class='num'>
+        <span v-for='(b, idx) in blankRight' :key='"blankR"+idx'>{{ ' ' }}</span>
         <span
           v-for='(l, j) in rightC' :key='j'
-          :class='{"checked": isChecked(j, "right"), "between": isBetween(j, "right")}'
-          class='cal-item' @click='checkDay(j, "right")'>{{ l }}</span>
+          :class='{"checked": isChecked(nextMonth === "01" ? year + 1 : year, Number(nextMonth), j), "between": isBetween(nextMonth === "01" ? year + 1 : year, Number(nextMonth), j)}'
+          class='cal-item' @click='checkDay(nextMonth === "01" ? year + 1 : year, Number(nextMonth), j)'>{{ l }}</span>
       </div>
     </div>
   </div>
@@ -34,75 +39,114 @@
 <script>
 import IconPrev from 'assets/svg/IconPrev'
 import IconNext from 'assets/svg/IconNext'
-import { ref } from '@nuxtjs/composition-api'
+import { computed, ref } from '@nuxtjs/composition-api'
 
 export default {
   name: 'CalendarContainer',
   components: { IconNext, IconPrev },
   setup() {
     const date = new Date()
-    const year = date.getFullYear()
+    const year = ref(date.getFullYear())
+
     const getMonth = (month) => {
       if (month < 10) {
         return `0${month}`
-      } else if (month === 11) {
-        return '01'
       } else {
         return month
       }
     }
-    const month = getMonth(date.getMonth() + 1)
-    const nextMonth = getMonth(parseInt(month, 10) + 1)
-    const parsedMonth = (month) => {
-      if (month === 0) {
-        return 11
-      } else {
-        return month
-      }
-    }
-    const parsed = new Date(year, parsedMonth(parseInt(month, 10) - 1)).getDay()
-    console.log((parseInt(month, 10)), parsed)
 
-    const leftC = Array.from({ length: 30 }, (v, i) => i + 1)
-    const rightC = Array.from({ length: 31 }, (v, i) => i + 1)
-    const days = ['일', '월', '화', '수', '목', '금', '토']
-    const firstChecked = ref({ idx: -1 })
-    const secondChecked = ref({ idx: -1 })
-    const checkDay = (idx, direction) => {
-      if (secondChecked.value.idx !== -1) {
-        // 둘 다 초기화
-        firstChecked.value.idx = -1
-        secondChecked.value.idx = -1
-      }
-      if (firstChecked.value.idx === -1) {
-        firstChecked.value.idx = idx
-        firstChecked.value.direction = direction
+
+    const dateMonth = ref(date.getMonth() + 1) // 3월이라면, 3
+    const month = ref(computed(() => getMonth(dateMonth.value)))
+    const nextMonth = ref(computed(() => getMonth(dateMonth.value + 1 === 13 ? 1 : dateMonth.value + 1)))
+    const blankLeft = ref(computed(() => new Date(year.value, dateMonth.value - 1).getDay()))
+    const blankRight = ref(computed(() => new Date(year.value, dateMonth.value).getDay()))
+    const leftC = ref(computed(() => Array.from({ length: new Date(year.value, dateMonth.value, 0).getDate() }, (v, i) => i + 1)))
+    const rightC = ref(computed(() => Array.from({ length: new Date(year.value, dateMonth.value + 1, 0).getDate() }, (v, i) => i + 1)))
+
+    const goPrev = () => {
+      // month가 1씩 줄고
+      // 일수가 일씩 줄고
+      // secondChecked.value.idx가 first~가
+      if (dateMonth.value === 1) {
+        year.value -= 1
+        dateMonth.value = 12
       } else {
-        secondChecked.value.idx = idx
-        secondChecked.value.direction = direction
+        dateMonth.value -= 1
+      }
+
+    }
+    const goNext = () => {
+      // month가 1씩 줄고
+      // 일수가 일씩 줄고
+      // secondChecked.value.idx가 first~가
+      if (dateMonth.value === 12) {
+        year.value += 1
+        dateMonth.value = 1
+      } else {
+        dateMonth.value += 1
+      }
+
+    }
+    const days = ['일', '월', '화', '수', '목', '금', '토']
+    const firstChecked = ref({ year: year.value, month: month.value, day: -1 })
+    const secondChecked = ref({ year: year.value, month: nextMonth.value, day: -1 })
+
+    const checkDay = (year, month, day) => {
+      if (secondChecked.value.day !== -1) {
+        // 둘 다 초기화
+        firstChecked.value.day = -1
+        secondChecked.value.day = -1
+      }
+      if (firstChecked.value.day === -1) {
+        firstChecked.value.day = day
+        firstChecked.value.month = month
+        firstChecked.value.year = year
+        console.log(firstChecked.value)
+      } else {
+        secondChecked.value.day = day
+        secondChecked.value.month = month
+        secondChecked.value.year = year
+
         console.log(secondChecked.value)
       }
     }
-    const isChecked = (i, direction) => {
-      return (i === firstChecked.value.idx && direction === firstChecked.value.direction) || (i === secondChecked.value.idx && direction === secondChecked.value.direction)
+    const isChecked = (year, month, day) => {
+      return (day === firstChecked.value.day && month === firstChecked.value.month && year === firstChecked.value.year)
+        || (day === secondChecked.value.day && month === secondChecked.value.month && year === secondChecked.value.year)
     }
-    const isBetween = (i, direction) => {
-      if (secondChecked.value.idx !== -1) {
-        if (secondChecked.value.direction !== firstChecked.value.direction) {
-
-          if (direction === 'left') {
-            return i > firstChecked.value.idx
+    const isBetween = (year, m, day) => {
+      if (secondChecked.value.day !== -1) {
+        if (secondChecked.value.month !== firstChecked.value.month) {
+          // 서로 체크한 달이 다를 때
+          if (m === Number(month.value)) {
+            console.log(m, firstChecked.value.month)
+            return day > firstChecked.value.day
           } else {
-            console.log(i < secondChecked.value.idx)
-            return i < secondChecked.value.idx
+            return day < secondChecked.value.day
           }
         } else {
-          return (i > firstChecked.value.idx && i < secondChecked.value.idx) && direction === secondChecked.value.direction
+          return (day > firstChecked.value.day && day < secondChecked.value.day) && m === secondChecked.value.month
 
         }
       }
     }
-    return { leftC, days, checkDay, firstChecked, secondChecked, rightC, year, month, nextMonth, isChecked, isBetween }
+    return {
+      leftC,
+      days,
+      checkDay,
+      firstChecked,
+      secondChecked,
+      rightC,
+      year,
+      month,
+      nextMonth,
+      isChecked,
+      isBetween,
+      blankLeft,
+      blankRight, goPrev, goNext
+    }
   }
 }
 </script>
@@ -172,10 +216,12 @@ export default {
 
 .icon-right {
   margin-left: 12px;
+  cursor: pointer;
 }
 
 .icon-left {
   margin-right: 12px;
+  cursor: pointer;
 }
 
 .cal-item {
